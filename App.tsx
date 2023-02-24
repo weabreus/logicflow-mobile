1eed573d561b6be68f39c43ce9aa5b8eed4dcbcd
Wellington Abreu <wellington.abreu@gmail.com>
2023-02-24 01:52:15 -0400

    Added logic to update driver location

--- App.tsx
+++ App.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { StyleSheet } from "react-native";
 import { Provider as PaperProvider } from "react-native-paper";
 import { NavigationContainer } from "@react-navigation/native";
@@ -10,21 +10,62 @@ import Deliveries from "./components/Deliveries";
 import Settings from "./components/Settings";
 import DeliveryDetails from "./components/DeliveryDetails";
 import PickupDetails from "./components/PickupDetails";
+import * as Location from "expo-location";
 
 export default function App() {
+  useEffect(() => {
+    const updateCoords = async () => {
+      let { status } = await Location.getForegroundPermissionsAsync();
+      console.log(status)
+      if ( status === 'undetermined' ) {
+        let response = await Location.requestForegroundPermissionsAsync();
+
+        if (response.granted) {
+          // @ts-ignore
+          status = 'granted'
+        }
+      }
+
+      if (status === 'granted') {
+        let location = await Location.getCurrentPositionAsync({ });
+        sendLocation(location.coords.latitude, location.coords.longitude)
+        
+      }
+    };
+    updateCoords()
+    setInterval(updateCoords, 60000)
+  }, []);
+
+  const sendLocation = async (latitude: number, longitude: number) => {
+    const response = await fetch(
+      "http://192.168.5.102:3000/api/drivers/position/63f77a636ca022ad59149790",
+      {
+        method: "PATCH",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({ lat: latitude, lng: longitude }),
+      }
+    );
+
+    if (response.ok) {
+      console.log("Coords updated.");
+    } else {
+      console.log("Couldn't update the coordinates.");
+    }
+  };
+
   const Drawer = createDrawerNavigator();
   return (
     <PaperProvider>
-      
       <NavigationContainer>
-      <Navbar />
-        <Drawer.Navigator 
-          screenOptions={{headerShown: false, headerLeft: () => <MenuIcon />}}
-          drawerContent={(props) => <SideMenu {...props}/>}>
-            <Drawer.Screen name="Entregas" component={Deliveries}/>
-            <Drawer.Screen name="Ajustes" component={Settings}/>
-            <Drawer.Screen name="Detalles Entrega" component={DeliveryDetails}/>
-            <Drawer.Screen name="Detalles Recogido" component={PickupDetails}/>
+        <Navbar />
+        <Drawer.Navigator
+          screenOptions={{ headerShown: false, headerLeft: () => <MenuIcon /> }}
+          drawerContent={(props) => <SideMenu {...props} />}
+        >
+          <Drawer.Screen name="Entregas" component={Deliveries} />
+          <Drawer.Screen name="Ajustes" component={Settings} />
+          <Drawer.Screen name="Detalles Entrega" component={DeliveryDetails} />
+          <Drawer.Screen name="Detalles Recogido" component={PickupDetails} />
         </Drawer.Navigator>
       </NavigationContainer>
     </PaperProvider>
